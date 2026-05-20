89e3bde10b6032621330e40c07d3919b68976c6e
Óscar Sala <153222887+OscarSala96@users.noreply.github.com>
2026-05-20 12:36:54 +0200

    modulo de community y fixes notifybell

--- atalayas/lib/utils.ts
+++ atalayas/lib/utils.ts
@@ -125,7 +125,17 @@ export const API_ROUTES = {
     RESET: `${BASE_URL}/notifications/reset-count`,
     COUNT: `${BASE_URL}/notifications/unread-count`,
 
-  }
+  },
+
+  COMMUNITY: {
+    CREATE: `${BASE_URL}/community`,
+    CREATE_WITH_UPLOAD: `${BASE_URL}/community/upload`, // Para cuando el General Admin sube un archivo local
+    GET_ALL: `${BASE_URL}/community`,
+    GET_TYPES: `${BASE_URL}/community/tipos`,          // El endpoint para alimentar el autocompletado y los botones de filtro
+    GET_ONE: (id: string) => `${BASE_URL}/community/${id}`,
+    UPDATE: (id: string) => `${BASE_URL}/community/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/community/${id}`,
+  },
 };
 
 // Agrega esto al final de tu archivo @/lib/utils.ts
